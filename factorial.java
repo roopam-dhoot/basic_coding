d3f872d380dc90fd9941e794ada3150167e0e6c4
roopam-dhoot <roopamdhoot730@gmail.com>
2020-09-22 22:09:19 +0530

    factorial program

---
+++ factorial.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class factorial{
+    public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int fact=1;
+   for(int i=1;i<=a;i++){
+        fact=fact*i;
+    }
+    System.out.println(fact);
+}
+}
