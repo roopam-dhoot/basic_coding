86673212639c50da3867be71c9290014a62803b0
roopam-dhoot <roopamdhoot730@gmail.com>
2020-09-22 23:50:30 +0530

    even number odd number

---
+++ even_odd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class even_odd{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a%2==0){
+            System.out.println("Even number");
+        }
+        else {
+        System.out.println("odd number");
+        }
+        }
+}
