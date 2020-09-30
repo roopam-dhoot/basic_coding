2b6a1eef59907b6002e19e77f777716955b31217
roopam-dhoot <roopamdhoot730@gmail.com>
2020-09-30 23:20:46 +0530

    Implemented string replace program

--- Strings/remove_vowels.java
+++ Strings/remove_vowels.java
@@ -4,7 +4,7 @@ public class remove_vowels{
 
         StringBuffer  s=new StringBuffer ("hello world");
         Character a[]={'a','e','i','o','u','A','E','I','O','U'};
-        List<Character > v = Arrays.asList(a);
+        List<Character> v = Arrays.asList(a);
         for(int i=0;i<s.length();i++){
             if(v.contains(s.charAt(i))){
                 s.replace(i,i+1,"");
