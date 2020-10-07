acf7118246d9e212984a7e956e61c0310a20644a
roopam-dhoot <roopamdhoot730@gmail.com>
2020-10-07 20:05:21 +0530

    Implemented Ascii number for character

---
+++ Ascii_value_char.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Ascii_value_char{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        char c=s.next().charAt(0);
+        int ascii=c;
+        System.out.println(ascii);
+    }
+}
