edf0fde975e7ca79ed0d456a0b3b104a9bc281a1
roopam-dhoot <roopamdhoot730@gmail.com>
2020-10-08 18:06:58 +0530

    Implemented return excel column number for given string

---
+++ Excel_col_string.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Excel_col_string{
+    static int titleToNumber(String s)
+    {
+           int result = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            result *= 26;
+            result += s.charAt(i) - 'A' + 1;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter no of input do you have");
+        int num=s.nextInt();
+        String[] str=new String[num+1];
+        for(int i=0;i<str.length;i++){
+             str[i]=s.nextLine();
+        }
+
+            System.out.println(str.toString());
+
+        for(int j=1;j<str.length;j++){
+            System.out.println(titleToNumber(str[j]));
+        }
+    }
+}
