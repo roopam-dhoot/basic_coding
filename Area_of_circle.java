5667098210b1a627d68f677173dcd69a3bf690e6
roopam-dhoot <roopamdhoot730@gmail.com>
2020-10-09 10:46:27 +0530

    Implemented area of circle

---
+++ Area_of_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Area_of_circle{
+    public  static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+double res=3.14*num*num;
+System.out.println(res);
+    }
+}
